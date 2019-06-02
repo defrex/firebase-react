d0de2acb5115cfadda1baa6043b16ff347188834
Aron Jones <aron.jones@gmail.com>
2019-06-02 17:34:30 -0400

    move nav

--- ui/App.tsx
+++ ui/App.tsx
@@ -1,6 +1,19 @@
+import { Link } from '@reach/router'
 import React from 'react'
 import { Routes } from 'ui/routes'
 
 export function App() {
-  return <Routes />
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to='/'>Index</Link>
+        </li>
+        <li>
+          <Link to='/example'>Example</Link>
+        </li>
+      </ul>
+      <Routes />
+    </div>
+  )
 }
--- ui/routes/index.tsx
+++ ui/routes/index.tsx
@@ -1,23 +1,13 @@
-import { Link, Router } from '@reach/router'
+import { Router } from '@reach/router'
 import React from 'react'
 import { ExampleRoute } from 'ui/routes/Example'
 import { IndexRoute } from 'ui/routes/Index'
 
 export function Routes() {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to='/'>Index</Link>
-        </li>
-        <li>
-          <Link to='/example'>Example</Link>
-        </li>
-      </ul>
-      <Router>
-        <IndexRoute path='/' />
-        <ExampleRoute path='/example' />
-      </Router>
-    </div>
+    <Router>
+      <IndexRoute path='/' />
+      <ExampleRoute path='/example' />
+    </Router>
   )
 }
