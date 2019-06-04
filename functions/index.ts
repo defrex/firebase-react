4206cf6c0a678ea5a9c8a63d7f6473b7fb14aec8
Kristian F Jones <me@kristianjones.xyz>
2019-06-04 10:09:55 -0500

    feat: Move all API related files into functions/api.

--- functions/index.ts
+++ functions/index.ts
@@ -1,10 +1,14 @@
 import { https } from 'firebase-functions'
 import { hotUiServer } from './ui'
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { apiServer } from './api'
 =======
 import { APIServer } from './api'
 >>>>>>> feat: Add API Function.
+=======
+import { apiServer } from './api'
+>>>>>>> feat: Move all API related files into functions/api.
 
 export const hello = https.onRequest((req, res) => {
   res.send('Hello Functions')
@@ -17,7 +21,11 @@ export const ui = https.onRequest(async (req, res) => {
 })
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 export const api = https.onRequest(apiServer)
 =======
 export const api = https.onRequest(APIServer)
 >>>>>>> feat: Add API Function.
+=======
+export const api = https.onRequest(apiServer)
+>>>>>>> feat: Move all API related files into functions/api.
