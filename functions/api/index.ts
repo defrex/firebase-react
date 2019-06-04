492a4804de1daef046549cade13b9e3a407c6a81
Kristian Jones <kristianfjonesis@gmail.com>
2019-06-04 10:34:45 -0500

    feat: Add API Function. (#1)
    * feat: Add API Function.
    Uses TypeGraphQL for the schema creation and resolvers,
    Apollo-server-cloud-functions for the function handler
    
    * feat: Move all API related files into functions/api.
    
    * fix: Remove already declared libs from functions/tsconfig.json

---
+++ functions/api/index.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express'
+import { ApolloServer } from 'apollo-server-cloud-functions'
+import { buildSchema } from 'type-graphql'
+import { HelloWorldResolver } from './resolvers'
+
+export const apiServer = async (req: Request, res: Response) => {
+  const schema = await buildSchema({
+    resolvers: [HelloWorldResolver],
+  })
+
+  const apolloServer = new ApolloServer({
+    schema,
+    introspection: true,
+    playground: false,
+  })
+
+  return apolloServer.createHandler({ cors: { origin: '*' } })(req, res)
+}
