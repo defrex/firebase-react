045196fd2143dcf750868f19b4bd7ac7c261b5db
Aron Jones <aron.jones@gmail.com>
2019-06-07 20:29:32 -0400

    add a workbox strategy

--- ui/service-worker.ts
+++ ui/service-worker.ts
@@ -1,7 +1,3 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
 
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`)
-} else {
-  console.log(`Boo! Workbox didn't load 😬`)
-}
+workbox.routing.registerRoute(new RegExp('.*.js'), new workbox.strategies.NetworkFirst())
