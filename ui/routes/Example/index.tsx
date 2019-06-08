b4bfe9ee9f88e0c00577fe3d148f3c6ef28f14be
Kristian F Jones <me@kristianjones.xyz>
2019-06-08 09:55:40 -0500

    fix: Remove temp JSONLD demo

--- ui/routes/Example/index.tsx
+++ ui/routes/Example/index.tsx
@@ -1,32 +1,9 @@
 import { RouteComponentProps } from '@reach/router'
 import React from 'react'
 import { useTitle } from 'ui/components/HeadProvider'
-import { useBreadCrumbJsonLd } from 'ui/components/useSEO';
 
 export function ExampleRoute(props: RouteComponentProps) {
   useTitle('Example Page')
-  useBreadCrumbJsonLd([
-    {
-      position: 1,
-      name: 'Books',
-      item: 'https://example.com/books',
-    },
-    {
-      position: 2,
-      name: 'Authors',
-      item: 'https://example.com/books/authors',
-    },
-    {
-      position: 3,
-      name: 'Ann Leckie',
-      item: 'https://example.com/books/authors/annleckie',
-    },
-    {
-      position: 4,
-      name: 'Ancillary Justice',
-      item: 'https://example.com/books/authors/ancillaryjustice',
-    },
-  ])
   return (
     <div>
       <h1>Example</h1>
